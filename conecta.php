3e9717a3daa67275cbfd948a740f3c96f120a46f
Endi Monan <endi@mngs.com.br>
2020-11-03 09:53:11 -0300

    teste

--- conecta.php
+++ conecta.php
@@ -8,5 +8,5 @@ $db_name = "validahoras";
 $conexao = mysqli_connect($servername, $username, $password, $db_name);
 
 if(mysqli_connect_error()){
-    echo "Falha na conexão \n\n".mysqli_connect_error();
+    echo "Falha na conexão \n".mysqli_connect_error();
 }
