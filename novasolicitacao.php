02036af9cec93504ef131394ffe0413ef433b032
Endi Monan <endi@mngs.com.br>
2020-12-07 21:14:34 -0300

    falta estilizacao

--- novasolicitacao.php
+++ novasolicitacao.php
@@ -21,123 +21,119 @@ $idLogado = $_SESSION['id_usuario'];
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Horas Complementares</title>
-  <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <!-- chamando CSS -->
   <link rel="stylesheet" href="estilo.css">
-  <script type="text/javascript" src="jquery-1.11.0.min.js"></script>
 </head>
 
 
 <body>
-  <div class="topo-header">
-    <img class="logo" src="img/logo.png">
-    <p class="descricao">Plataforma para validação de atividades complementares</p>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-center">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup" style="display: flex; justify-content: center;">
+        <div class="navbar-nav text-center">
+          <a class="nav-item nav-link active" href="paginaUsuario.php">Principal<span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="novasolicitacao.php">Cadastrar nova solicitação</a>
+          <a class="nav-item nav-link" href="acompanhamentoSolicitacao.php">Acompanhamento de solicitações</a>
+          <a class="nav-item nav-link" href="aprovadas.php">Solicitações aprovadas</a>
+          <a class="nav-item nav-link" href="dadosUsuario.php">Dados do usuário</a>
+          <a class="nav-item nav-link" href="sair.php">Sair
+          </a>
+        </div>
+      </div>
+    </nav>
   </div>
-  <div class="row">
-    <div id="menu" class="menu col-3">
-      <nav>
-        <div class="link">
-          <a href="paginaUsuario.php">Principal</a>
-        </div>
-        <div class="link active">
-          <a href="novasolicitacao.php">Cadastrar nova solicitação</a>
-        </div>
-        <div class="link">
-          <a href="acompanhamentoSolicitacao.php">Acompanhamento de solicitações</a>
-        </div>
-        <div class="link">
-          <a href="aprovadas.php">Solicitações Aprovadas</a>
-        </div>
-        <div class="link">
-          <a href="dadosUsuario.php">Dados do usuário</a>
-        </div>
-        <div class="link">
-          <?php echo "<a href='sair.php'>Sair</a>"; ?>
-        </div>
 
-      </nav>
-    </div>
+  <div id="principal" class="">
+    <div class="jumbotron row justify-content-center align-self-center" style="height: 100vh;">
+      <!-- <h5 class="mt-n4 pb-5">Lembre-se, a modalidade selecionada deve ter relação com a atividade.</h5> -->
+      <div class="my-5">
+        <form method="POST" action="salvarHoras.php" class="modalidade">
+          <div class="row ">
+            <div id="modalidade" class="text-center">
 
-    <div id="principal" class="principal col-9">
-      <form method="POST" action="salvarHoras.php" class="modalidade" >
-        <h5><i>Lembre-se, a modalidade selecionada deve ter relação com a atividade.</i></h5>
+              <label class="text-modalidade">Modalidade desejada*</label>
 
-        <div class="row">
-          <div id="modalidade" class="modalidade-content">
-            <label class="text-modalidade">Modalidade desejada*</label>
+              <label class="form-check">
+                <input type="radio" id="modalidade" name="modalidade" value="Ensino"><span class="label label-success"> Ensino</span>
+              </label>
 
-            <label class="form-check">
-              <input type="radio" id="modalidade" name="modalidade" value="Ensino"><span class="label label-success"> Ensino</span>
-            </label>
+              <label class="form-check">
+                <input type="radio" id="modalidade" name="modalidade" value="Pesquisa"><span class="label label-success"> Pesquisa</span>
+              </label>
 
-            <label class="form-check">
-              <input type="radio" id="modalidade" name="modalidade" value="Pesquisa"><span class="label label-success"> Pesquisa</span>
-            </label>
+              <label class="form-check">
+                <input type="radio" id="modalidade" name="modalidade" value="Extensão"><span class="label label-success"> Extensão</span>
+              </label>
 
-            <label class="form-check">
-              <input type="radio" id="modalidade" name="modalidade" value="Extensão"><span class="label label-success"> Extensão</span>
-            </label>
+            </div>
 
+            <div class="form-group align-self-center">
+              <select style="width: 575px;" name="titulo" class="custom-select">
+                <option disabled selected>Atividade</option>
+                <option style="color: #ACDFBA;" disabled>ENSINO</option>
+
+                <option value="Aula inaugural 4h">Aula inaugural 4Hrs</option>
+                <option value="Unidade Curricular não prevista na organização curricular do curso, max 50h">Unidade Curricular não prevista na organização curricular do curso, max 50h</option>
+                <option value="Monitoria acadêmica, max 40h">Monitoria acadêmica, max 40h</option>
+                <option value="Visita técnica, max 20h">Visita técnica, max 20h</option>
+                <option value="Ministrante de curso, max 30h">Ministrante de curso, max 30h</option>
+                <option value="Participação em palestra, 3h por palestra">Participação em palestra, 3h por palestra</option>
+                <option value="Participação em projetos integradores, 20h">Participação em projetos integradores, 20h</option>
+
+                <option style="color: #ACDFBA;" disabled>PESQUISA</option>
+                <option value="Participação em projeto de pesquisa, max 40h">Participação em projeto de pesquisa, max 40h</option>
+                <option value="Participações em congressos, seminários, simpósios, 40h por Eventos">Participações em congressos, seminários, simpósios, 40h por Eventos</option>
+                <option value="Publicação de artigos, 40h por artigo">Publicação de artigos, 40h por artigo</option>
+                <option value="Publicação de resumos, 40h por resumo">Publicação de resumos, 40h por resumo</option>
+                <option value="Autoria ou co-autoria, 40h">Autoria ou co-autoria, 40h</option>
+
+                <option style="color: #ACDFBA;" disabled>EXTENSÃO</option>
+
+                <option value="Estágio, max 50h">Estágio, max 50h</option>
+                <option value="Participação de eventos, max 10h">Participação de eventos, max 10h</option>
+                <option value="Participação em congressos, 10h">Participação em congressos, 10h</option>
+                <option value="Participações em congressos, seminários, simpósios, 5h por dia">Participações em congressos, seminários, simpósios, 5h por dia</option>
+                <option value="Participações de cursos de extensão, max 40h">Participações de cursos de extensão, max 40h</option>
+                <option value="Representação estudantil, max 10h">Representação estudantil, max 10h</option>
+                <option value="Representante de turma, max 20h">Representante de turma, max 20h</option>
+                <option value="Curso de linguas, max 40h">Curso de linguas, max 40h</option>
+
+              </select>
+            </div>
+            </div>
+         
+              <div class="text-center">
+                <div class="col-4 p-5">
+                  <label for="upload">Arquivo para upload*</label>
+                  <input name="upload" type="file" class="form-control-file">
+                </div>
+
+                <div class="form-check">
+                  <input name="checkbox" type="checkbox" class="form-check-input">
+                  <label name="semUpload" class="form-check-label">Sem upload de arquivo</label>
+                </div>
+              </div>
+          
+      
+
+          <div style="width: 500px;" class="form-group">
+            <label name="descricao">Descrição</label>
+            <textarea name="descricao" class="form-control" id="descricao" rows="3"></textarea>
           </div>
-        </div>
-        <div class="form-group">
-          <select style="width: 500px;" name="titulo" class="custom-select">
-            <option disabled selected>Atividade</option>
-            <option style="color: #ACDFBA;" disabled>ENSINO</option>
 
-            <option value="Aula inaugural 4h">Aula inaugural 4Hrs</option>
-            <option value="Unidade Curricular não prevista na organização curricular do curso, max 50h">Unidade Curricular não prevista na organização curricular do curso, max 50h</option>
-            <option value="Monitoria acadêmica, max 40h">Monitoria acadêmica, max 40h</option>
-            <option value="Visita técnica, max 20h">Visita técnica, max 20h</option>
-            <option value="Ministrante de curso, max 30h">Ministrante de curso, max 30h</option>
-            <option value="Participação em palestra, 3h por palestra">Participação em palestra, 3h por palestra</option>
-            <option value="Participação em projetos integradores, 20h">Participação em projetos integradores, 20h</option>
-
-            <option style="color: #ACDFBA;" disabled>PESQUISA</option>
-            <option value="Participação em projeto de pesquisa, max 40h">Participação em projeto de pesquisa, max 40h</option>
-            <option value="Participações em congressos, seminários, simpósios, 40h por Eventos">Participações em congressos, seminários, simpósios, 40h por Eventos</option>
-            <option value="Publicação de artigos, 40h por artigo">Publicação de artigos, 40h por artigo</option>
-            <option value="Publicação de resumos, 40h por resumo">Publicação de resumos, 40h por resumo</option>
-            <option value="Autoria ou co-autoria, 40h">Autoria ou co-autoria, 40h</option>
-
-            <option style="color: #ACDFBA;" disabled>EXTENSÃO</option>
-
-            <option value="Estágio, max 50h">Estágio, max 50h</option>
-            <option value="Participação de eventos, max 10h">Participação de eventos, max 10h</option>
-            <option value="Participação em congressos, 10h">Participação em congressos, 10h</option>
-            <option value="Participações em congressos, seminários, simpósios, 5h por dia">Participações em congressos, seminários, simpósios, 5h por dia</option>
-            <option value="Participações de cursos de extensão, max 40h">Participações de cursos de extensão, max 40h</option>
-            <option value="Representação estudantil, max 10h">Representação estudantil, max 10h</option>
-            <option value="Representante de turma, max 20h">Representante de turma, max 20h</option>
-            <option value="Curso de linguas, max 40h">Curso de linguas, max 40h</option>
-
-          </select>
-        </div>
-
-
-        <div class="form-group">
-          <label for="upload">Arquivo para upload*</label>
-          <input name="upload" type="file" class="form-control-file">
-
-          <div class="form-group form-check">
-            <input name="checkbox" type="checkbox" class="form-check-input">
-            <label name="semUpload" class="form-check-label">Sem upload de arquivo</label>
+          <div class="btn-group">
+            <button style="width:150px;" type="reset" class="btn btn-secondary">Limpar</button>
+            <button style="width:150px;" type="submit" name="btnHoras" class="btn btn-primary">Enviar</button>
           </div>
-        </div>
-
-        <div style="width: 500px;" class="form-group">
-          <label name="descricao">Descrição</label>
-          <textarea name="descricao" class="form-control" id="descricao" rows="3"></textarea>
-        </div>
-
-        <div class="btn-group">
-          <button style="width:150px;" type="reset" class="btn btn-secondary">Limpar</button>
-          <button style="width:150px;" type="submit" name="btnHoras" class="btn btn-primary">Enviar</button>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   </div>
+
+
 
   <footer>
     <p>
--- paginaUsuario.php
+++ paginaUsuario.php
@@ -20,83 +20,72 @@ $idLogado = $_SESSION['id_usuario'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Horas Complementares</title>
-    <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <!-- chamando CSS -->
     <link rel="stylesheet" href="estilo.css">
 </head>
 
 
 <body>
-    <div class="topo-header">
-        <img class="logo" src="img/logo.png">
-        <p class="descricao">Plataforma para validação de atividades complementares</p>
-    </div>
     <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-center">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup" style="display: flex; justify-content: center;">
+                <div class="navbar-nav text-center">
+                    <a class="nav-item nav-link active" href="paginaUsuario.php">Principal<span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="novasolicitacao.php">Cadastrar nova solicitação</a>
+                    <a class="nav-item nav-link" href="acompanhamentoSolicitacao.php">Acompanhamento de solicitações</a>
+                    <a class="nav-item nav-link" href="aprovadas.php">Solicitações aprovadas</a>
+                    <a class="nav-item nav-link" href="dadosUsuario.php">Dados do usuário</a>
+                    <a class="nav-item nav-link" href="sair.php">Sair
+                    </a>
+                </div>
+            </div>
+        </nav>
     </div>
+
     <div class="row">
-        <div id="menu" class="menu col-3">
-            <nav>
-                <div class="link active">
-                    <a href="paginaUsuario.php">Principal</a>
-                </div>
-                <div class="link">
-                    <a href="novasolicitacao.php">Cadastrar nova solicitação</a>
-                </div>
-                <div class="link">
-                    <a href="acompanhamentoSolicitacao.php">Acompanhamento de solicitações</a>
-                </div>
-                <div class="link">
-                    <a href="aprovadas.php">Solicitações Aprovadas</a>
-                </div>
-                <div class="link">
-                    <a href="dadosUsuario.php">Dados do usuário</a>
-                </div>
-                <div class="link">
-                    <?php echo "<a href='sair.php'>Sair</a>"; ?>
-                </div>
+        <div id="principal" class="w-100">
+            <div class="jumbotron text-center" style="height: 100vh;">
+                <h1 class="display-4">
+                    <?php
+                    if (!empty($_SESSION['id_usuario'])) {
+                        echo "Olá " . $_SESSION['nome'] . ",";
+                    } else {
+                        $_SESSION['msg'] = "Você precisa estar logado";
+                        header("Location: index.php");
+                    }
+                    ?>
 
-            </nav>
+                    <p class="lead my-5">Seja bem vindo à Plataforma de Validação de Atividades Complementares.
+                        Aqui você poderá cadastrar solicitações para validar Atividades Acadêmicas Complementares (AAC) elegíveis à graduação em Análise e Desenvolvimento de Sistemas. É possível cadastrar as seguintes modalidades:</p>
+
+                    <h3>1) Ensino</h3>
+                    <h3>2) Pesquisa</h3>
+                    <h3>3) Extensão</h3>
+                    <br>
+
+                    <p class="lead my-5">Acesse o menu lateral para cadastrar uma nova solicitação e anexar os documentos. Lembre-se que, apesar da plataforma aceitar arquivos em formato de imagens (JPEG, PNG e TIFF), será aceito apenas um arquivo por solicitação. Se a sua documentação possuir mais de uma página, converta elas para um único pdf.
+                        Acompanhe o status de suas solicitações pelo menu lateral.
+                        Lembre-se que não é possível reabrir solicitações: se necessário, cadastre uma nova.</p>
+                    <p class="lead my-5"><strong>Em caso de dúvidas, contate a coordenação do curso através do email tiago.asp@senai.com.br.
+                        </strong></p>
+                    <hr class="my-4">
+                    <a class="btn btn-primary btn-lg" style="width: 30%;" href="novasolicitacao.php"
+                        role="button">Cadastrar nova solicitação</a>
+            </div>
         </div>
 
-        <div id="principal" class="principal col-9">
-            <h4 style="text-align: left;">
-                <?php
+        <footer>
+            <p>
+                Federação das Indústrias do Estado de Santa Catarina <br>
+                Departamento Regional - Fone: 48 3231 4100 <br>
+                Rod. Admar Gonzaga, 2765 - Florianópolis/SC - 88034-001
+            </p>
 
-                if (!empty($_SESSION['id_usuario'])) {
-                    echo "Olá " . $_SESSION['nome']. ",";
-                } else {
-                    $_SESSION['msg'] = "Você precisa estar logado";
-                    header("Location: index.php");
-                }
-                ?>
+        </footer>
 
-            </h4><br>
-
-            <p>Seja bem vindo à Plataforma de Validação de Atividades Complementares.
-                Aqui você poderá cadastrar solicitações para validar Atividades Acadêmicas Complementares (AAC) elegíveis à graduação em Análise e Desenvolvimento de Sistemas. É possível cadastrar as seguintes modalidades:</p>
-
-            <h6>1) Ensino;</h6>
-            <h6>2) Pesquisa;</h6>
-            <h6>3) Extensão;</h6><br>
-
-            <p>Acesse o menu lateral para cadastrar uma nova solicitação e anexar os documentos. Lembre-se que, apesar da plataforma aceitar arquivos em formato de imagens (JPEG, PNG e TIFF), será aceito apenas um arquivo por solicitação. Se a sua documentação possuir mais de uma página, converta elas para um único pdf.
-                Acompanhe o status de suas solicitações pelo menu lateral.
-                Lembre-se que não é possível reabrir solicitações: se necessário, cadastre uma nova.</p>
-            <p><strong>Em caso de dúvidas, contate a coordenação do curso através do email tiago.asp@senai.com.br.
-                </strong></p>
-
-        </div>
-    </div>
-
-    <footer>
-        <p>
-            Federação das Indústrias do Estado de Santa Catarina <br>
-            Departamento Regional - Fone: 48 3231 4100 <br>
-            Rod. Admar Gonzaga, 2765 - Florianópolis/SC - 88034-001
-        </p>
-
-    </footer>
 </body>
 
 </html>
