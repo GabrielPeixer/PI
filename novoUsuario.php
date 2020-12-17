9a3e1b21ca0de1c8c054ac40ffd301ca04ecccc7
Endi Monan <endi@mngs.com.br>
2020-12-17 20:02:08 -0300

    novousuario alterado

--- novoUsuario.php
+++ novoUsuario.php
@@ -32,53 +32,54 @@ $usuario = mysqli_query($conexao, $sql);
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Horas Complementares</title>
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <!-- chamando CSS -->
-  <link rel="stylesheet" href="estilo.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horas Complementares</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilo.css">
+
+    <script>
+        function verifica() {
+            const select = document.getElementById('aprovado');
+            const optionSelected = select.options[select.selectedIndex].value;
+            if (optionSelected === 'Aprovado') {
+                document.getElementById('horas').style.display = "block";
+            } else {
+                document.getElementById('horas').style.display = "none";
+            }
+        }
+    </script>
 </head>
 
 
 <body>
-  <div class="topo-header">
-    <img class="logo" src="logo.png">
-    <p class="descricao">Plataforma para validação de atividades complementares</p>
-  </div>
-  <div class="row">
-    <div id="menu" class="menu col-3">
-    <nav>
-                <div class="link">
-                    <a href="paginaAdm.php">Principal</a>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-center">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup" style="display: flex; justify-content: center;">
+                <div class="navbar-nav text-center">
+                    <a class="nav-item nav-link " href="paginaAdm.php">Principal<span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="acompanhamentoAdm.php">Acompanhar Solicitações</a>
+                    <a class="nav-item nav-link " href="gerenciamento.php">Gerenciar Solicitações</a>
+                    <a class="nav-item nav-link" href="relatorios.php">Relatorios</a>
+                    <a class="nav-item nav-link active" href="sistema.php">Sistema</a>
+                    <a class="nav-item nav-link" href="sair.php">Sair
+                    </a>
                 </div>
-                <div class="link">
-                    <a href="acompanhamentoAdm.php">Acompanhamento de solicitações</a>
-                </div>
-                <div class="link">
-                    <a href="gerenciamento.php">Gerenciamento de solicitações</a>
-                </div>
-                <div class="link">
-                    <a href="relatorios.php">Relatorios</a>
-                </div>
-                <div class="link active">
-                    <a href="sistema.php">sistema</a>
-                </div>
-                <div class="link">
-                    <?php echo "<a href='sair.php'>Sair</a>"; ?>
-                </div>
-
-            </nav>
+            </div>
+        </nav>
     </div>
 
-    <div id="principal" class="principal col-9">
+    <div id="principal" class=" jumbotron d-flex justify-content-center col-12 p-5 mt-5">
 
-      <div id="cadastro" class="card border-secondary mb-3" style="left: 450px; top: 190px; max-width: 18rem; padding: 1.25rem; text-align: center;">
+      <div id="cadastro" class="card border-secondary mb-3" style="max-width: 18rem; padding: 1.25rem; text-align: center;">
         <?php
         if ($horasAluno = true) {
           echo "Usuario ".$nome." cadastrado com sucesso!"; ?>
